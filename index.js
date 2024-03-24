5a291912e1317f4f4b0c91564ccd57cd6f52418c
Badr Aliloui <tranquil2021@hotmail.com>
2024-03-24 20:12:16 +0000

    Add a picture, an id for the player1, Add the choice randomly of the sign  for the computer

--- index.js
+++ index.js
@@ -1,75 +1,74 @@
-
-
-
-let randomNumber2 = Math.floor(Math.random() * 5) + 1;
+let randomNumber2 = 0;
 let image2 = "./images/dice" + randomNumber2 + ".png";
+document.querySelector("#computer img").setAttribute("src", image2);
 
-let player2 = document
-  .querySelector("#computer img")
-  .setAttribute("src", image2);
-console.log(player2);
+let number;
 
-let buttons = document.querySelectorAll("button[data-choice]");
-
-for (let button of buttons) {
-  button.addEventListener("click", function () {
-    let choice = this.getAttribute("data-choice");
-    let randomNumber1;
-    switch (choice) {
-      case "rock":
-        randomNumber1 = 1;
-        break;
-      case "scissors":
-        randomNumber1 = 2;
-        break;
-      case "paper":
-        randomNumber1 = 3;
-        break;
-      case "lizard":
-        randomNumber1 = 4;
-        break;
-      case "spock":
-        randomNumber1 = 5;
-        break;
-      default:
-        randomNumber1 = 1;
-    }
-    let image1 = "./images/dice" + randomNumber1 + ".png";
-    document
-  .querySelector("#player1 img")
-  .setAttribute("src", image1);
-  
-    
-  });
-  
+function choiceSign() {
+  let buttons = document.querySelectorAll("button[data-choice]");
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      let choice = this.getAttribute("data-choice");
+      let number;
+      switch (choice) {
+        case "rock":
+          number = 1;
+          break;
+        case "scissors":
+          number = 2;
+          break;
+        case "paper":
+          number = 3;
+          break;
+        case "lizard":
+          number = 4;
+          break;
+        case "spock":
+          number = 5;
+          break;
+        default:
+          number = 0;
+      }
+      let image1 = "./images/dice" + number + ".png";
+      document.querySelector("#player1 img").setAttribute("src", image1);
+    });
+  }
 }
 
 function runGame() {
-  if (randomNumber1 === 1 && (randomNumber2 === 2 || randomNumber2 === 4)) {
+  if (number === 1 && (randomNumber2 === 2 || randomNumber2 === 4)) {
     document.querySelector("h1").innerHTML = "Player1 wins!";
-  } else if (
-    randomNumber1 === 2 &&
-    (randomNumber2 === 3 || randomNumber2 === 4)
-  ) {
+  } else if (number === 2 && (randomNumber2 === 3 || randomNumber2 === 4)) {
     document.querySelector("h1").innerHTML = "Player1 wins!";
-  } else if (
-    randomNumber1 === 3 &&
-    (randomNumber2 === 1 || randomNumber2 === 5)
-  ) {
+  } else if (number === 3 && (randomNumber2 === 1 || randomNumber2 === 5)) {
     document.querySelector("h1").innerHTML = "Player1 wins!";
-  } else if (
-    randomNumber1 === 4 &&
-    (randomNumber2 === 5 || randomNumber2 === 3)
-  ) {
+  } else if (number === 4 && (randomNumber2 === 5 || randomNumber2 === 3)) {
     document.querySelector("h1").innerHTML = "Player1 wins!";
-  } else if (
-    randomNumber1 === 5 &&
-    (randomNumber2 === 1 || randomNumber2 === 2)
-  ) {
+  } else if (number === 5 && (randomNumber2 === 1 || randomNumber2 === 2)) {
     document.querySelector("h1").innerHTML = "Player1 wins!";
-  } else if (randomNumber1 === randomNumber2) {
+  } else if (number === randomNumber2) {
     document.querySelector("h1").innerHTML = "Draw!";
   } else {
     document.querySelector("h1").innerHTML = "Computer wins!";
   }
 }
+
+function generateChoiceComputer() {
+  let randomNumber2 = Math.floor(Math.random() * 5) + 1;
+  let image2 = "./images/dice" + randomNumber2 + ".png";
+  document.querySelector("#computer img").setAttribute("src", image2);
+}
+
+function submitChoice() {
+  let Submission = document.querySelector(".button-submit button");
+  Submission.addEventListener("click", function () {
+    generateChoiceComputer();
+    runGame();
+  });
+}
+choiceSign();
+submitChoice();
+
+// function incrementScore(){
+
+// }
