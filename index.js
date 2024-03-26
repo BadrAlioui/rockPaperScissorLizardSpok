c84552fbb457dc2d8c814d3ccff2969d967cf19f
Badr Aliloui <tranquil2021@hotmail.com>
2024-03-26 12:13:47 +0000

    Add resetComputer to reset the image when the payer choose another sign, Add the option keypress Enter and change the functions

--- index.js
+++ index.js
@@ -1,52 +1,77 @@
-let randomNumber2 = 0;
-let image2 = './images/dice' + randomNumber2 + '.png';
-document.querySelector('#computer img').setAttribute('src', image2);
+let computerChoice = 0;
+let playerChoice = 0;
 
-let number = 0;
+function resetComputer() {
+    computerChoice = 0;
+    displayChoice('computer');
+}
+
+function displayChoice(actor) {
+    if (actor === 'player') {
+        let image1 = './images/dice' + playerChoice + '.png';
+        document
+            .querySelector('#' + actor + ' img')
+            .setAttribute('src', image1);
+    } else {
+        const image2 = './images/dice' + computerChoice + '.png';
+        document
+            .querySelector('#' + actor + ' img')
+            .setAttribute('src', image2);
+    }
+}
+// let image1 = './images/dice' + number + '.png';
+// document.querySelector('#player img').setAttribute('src', image1);
 
 function choiceSign() {
-    let buttons = document.querySelectorAll('button[data-choice]');
-    for (let button of buttons) {
-        button.addEventListener('click', function () {
-            let choice = this.getAttribute('data-choice');
+    let choice = this.getAttribute('data-choice');
 
-            switch (choice) {
-                case 'rock':
-                    number = 1;
-                    break;
-                case 'scissors':
-                    number = 2;
-                    break;
-                case 'paper':
-                    number = 3;
-                    break;
-                case 'lizard':
-                    number = 4;
-                    break;
-                case 'spock':
-                    number = 5;
-                    break;
-                default:
-                    number = 0;
-            }
-            let image1 = './images/dice' + number + '.png';
-            document.querySelector('#player1 img').setAttribute('src', image1);
-        });
+    switch (choice) {
+        case 'rock':
+            playerChoice = 1;
+            break;
+        case 'scissors':
+            playerChoice = 2;
+            break;
+        case 'paper':
+            playerChoice = 3;
+            break;
+        case 'lizard':
+            playerChoice = 4;
+            break;
+        case 'spock':
+            playerChoice = 5;
+            break;
+        default:
+            playerChoice = 0;
     }
+    displayChoice('player');
+    resetComputer();
 }
 
 function runGame() {
-    if (number === 1 && (randomNumber2 === 2 || randomNumber2 === 4)) {
+    if (playerChoice === 1 && (computerChoice === 2 || computerChoice === 4)) {
         document.querySelector('h1').innerHTML = 'Player1 wins!';
-    } else if (number === 2 && (randomNumber2 === 3 || randomNumber2 === 4)) {
+    } else if (
+        playerChoice === 2 &&
+        (computerChoice === 3 || computerChoice === 4)
+    ) {
         document.querySelector('h1').innerHTML = 'Player1 wins!';
-    } else if (number === 3 && (randomNumber2 === 1 || randomNumber2 === 5)) {
+    } else if (
+        playerChoice === 3 &&
+        (computerChoice === 1 || computerChoice === 5)
+    ) {
         document.querySelector('h1').innerHTML = 'Player1 wins!';
-    } else if (number === 4 && (randomNumber2 === 5 || randomNumber2 === 3)) {
+    } else if (
+        playerChoice === 4 &&
+        (computerChoice === 5 || computerChoice === 3)
+    ) {
         document.querySelector('h1').innerHTML = 'Player1 wins!';
-    } else if (number === 5 && (randomNumber2 === 1 || randomNumber2 === 2)) {
+    } else if (
+        playerChoice === 5 &&
+        (computerChoice === 1 || computerChoice === 2)
+    ) {
         document.querySelector('h1').innerHTML = 'Player1 wins!';
-    } else if (number === randomNumber2) {
+    } else if (playerChoice === computerChoice) {
         document.querySelector('h1').innerHTML = 'Draw!';
     } else {
         document.querySelector('h1').innerHTML = 'Computer wins!';
@@ -54,21 +79,35 @@ function runGame() {
 }
 
 function generateChoiceComputer() {
-    randomNumber2 = Math.floor(Math.random() * 5) + 1;
-    image2 = './images/dice' + randomNumber2 + '.png';
-    document.querySelector('#computer img').setAttribute('src', image2);
+    computerChoice = Math.floor(Math.random() * 5) + 1;
+    displayChoice('computer');
 }
 
 function submitChoice() {
-    let submitButton = document.querySelector('.button-submit button');
-    submitButton.addEventListener('click', function () {
-        generateChoiceComputer();
-        runGame();
-    });
+    generateChoiceComputer();
+    runGame();
 }
-choiceSign();
-submitChoice();
 
+document.addEventListener('DOMContentLoaded', function () {
+    let buttons = document.querySelectorAll('button[data-choice]');
+    for (let button of buttons) {
+        button.addEventListener('click', choiceSign);
+    }
+
+    document
+        .querySelector('#gameForm')
+        .addEventListener('submit', function (event) {
+            event.preventDefault();
+            submitChoice();
+        });
+});
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        submitChoice();
+    }
+});
 // function incrementScore(){
 
 // }
