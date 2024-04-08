bdfba95d655d78a3f146cdf39df8ebdd2c379f0d
Badr Aliloui <tranquil2021@hotmail.com>
2024-04-08 09:20:57 +0000

    To make the timer work

--- index.js
+++ index.js
@@ -1,142 +1,135 @@
-let computerChoice = 0;
 let playerChoice = 0;
+let computerChoice;
 
-function resetComputer() {
-    computerChoice = 0;
-    displayChoice('computer');
+function computerPlay() {
+    computerChoice = Math.floor(Math.random() * 5) + 1;
+    document
+        .querySelector('#computer img')
+        .setAttribute('src', './images/dice' + computerChoice + '.png');
 }
-
-function displayChoice(actor) {
-    if (actor === 'player') {
-        let image1 = './images/dice' + playerChoice + '.png';
-        document
-            .querySelector('#' + actor + ' img')
-            .setAttribute('src', image1);
-    } else {
-        const image2 = './images/dice' + computerChoice + '.png';
-        document
-            .querySelector('#' + actor + ' img')
-            .setAttribute('src', image2);
-    }
-}
-// let image1 = './images/dice' + number + '.png';
-// document.querySelector('#player img').setAttribute('src', image1);
 
 function choiceSign() {
-    let choice = this.getAttribute('data-choice');
-
-    switch (choice) {
-        case 'rock':
-            playerChoice = 1;
-            break;
-        case 'scissors':
-            playerChoice = 2;
-            break;
-        case 'paper':
-            playerChoice = 3;
-            break;
-        case 'lizard':
-            playerChoice = 4;
-            break;
-        case 'spock':
-            playerChoice = 5;
-            break;
-        default:
-            playerChoice = 0;
+    let buttons = document.querySelectorAll('button[data-choice]');
+    for (let button of buttons) {
+        button.addEventListener('click', function () {
+            switch (button.getAttribute('data-choice')) {
+                case 'rock':
+                    playerChoice = 1;
+                    break;
+                case 'scissors':
+                    playerChoice = 2;
+                    break;
+                case 'paper':
+                    playerChoice = 3;
+                    break;
+                case 'lizard':
+                    playerChoice = 4;
+                    break;
+                case 'spock':
+                    playerChoice = 5;
+                    break;
+            }
+            let image1 = './images/dice' + playerChoice + '.png';
+            let imagePlayer = document.querySelector('#player img');
+            imagePlayer.setAttribute('src', image1);
+            computerPlay();
+            runGame();
+        });
     }
-    displayChoice('player');
-    resetComputer();
 }
 
 function runGame() {
     if (playerChoice === 1 && (computerChoice === 2 || computerChoice === 4)) {
-        document.querySelector('h1').innerHTML = '🚩 Player wins!';
+        document.querySelector('header #result').innerHTML = '🚩 Player wins!';
         incrementScore();
     } else if (
         playerChoice === 2 &&
         (computerChoice === 3 || computerChoice === 4)
     ) {
-        document.querySelector('h1').innerHTML = '🚩 Player Wins!';
+        document.querySelector('header #result').innerHTML = '🚩 Player Wins!';
         incrementScore();
     } else if (
         playerChoice === 3 &&
         (computerChoice === 1 || computerChoice === 5)
     ) {
-        document.querySelector('h1').innerHTML = '🚩 Player Wins!';
+        document.querySelector('header #result').innerHTML = '🚩 Player Wins!';
         incrementScore();
     } else if (
         playerChoice === 4 &&
         (computerChoice === 5 || computerChoice === 3)
     ) {
-        document.querySelector('h1').innerHTML = '🚩 Player Wins!';
+        document.querySelector('header #result').innerHTML = '🚩 Player Wins!';
         incrementScore();
     } else if (
         playerChoice === 5 &&
         (computerChoice === 1 || computerChoice === 2)
     ) {
-        document.querySelector('h1').innerHTML = '🚩 Player Wins!';
+        document.querySelector('header #result').innerHTML = '🚩 Player Wins!';
         incrementScore();
     } else if (playerChoice === computerChoice) {
-        document.querySelector('h1').innerHTML = 'Draw!';
+        document.querySelector('header #result').innerHTML = 'Draw!';
     } else {
-        document.querySelector('h1').innerHTML = 'Computer Wins! 🚩';
+        document.querySelector('header #result').innerHTML =
+            'Computer Wins! 🚩';
         incrementLost();
     }
 }
 
-function generateChoiceComputer() {
-    computerChoice = Math.floor(Math.random() * 5) + 1;
-    displayChoice('computer');
-}
-
-function submitChoice() {
-    generateChoiceComputer();
-    runGame();
-}
-
 document.addEventListener('DOMContentLoaded', function () {
-    resetComputer();
-    let buttons = document.querySelectorAll('button[data-choice]');
-    for (let button of buttons) {
-        button.addEventListener('click', choiceSign);
-    }
-
+    document.querySelector('#play').addEventListener('click', start);
     document
-        .querySelector('#gameForm')
-        .addEventListener('submit', function (event) {
-            event.preventDefault();
-            submitChoice();
-        });
+        .querySelector('#computer img')
+        .setAttribute('src', './images/dice' + 0 + '.png');
+    choiceSign();
 });
 
-document.addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        submitChoice();
-    }
-});
+let oldScore;
+let oldLost;
 function incrementScore() {
-    let oldScore = parseInt(document.getElementById('score').innerText);
+    oldScore = parseInt(document.querySelector('#score').innerHTML);
     document.getElementById('score').innerText = ++oldScore;
-    if (oldScore === 3) {
-        document.querySelector('h1').innerHTML = 'You win!';
-        alert('Hey! You got it!Congratualtions!');
-        resetScore();
-    }
-}
-function incrementLost() {
-    let oldScore = parseInt(document.getElementById('incorrect').innerText);
-    document.getElementById('incorrect').innerText = ++oldScore;
-    if (oldScore === 3) {
-        document.querySelector('h1').innerHTML = 'Game Over!';
-        alert(
-            'Awwww.... you lost three times!Sorry It is the end of the game!'
-        );
-        resetScore();
-    }
 }
 
-function resetScore() {
-    document.getElementById('score').innerText = 0;
-    document.getElementById('incorrect').innerText = 0;
+function incrementLost() {
+    oldLost = parseInt(document.querySelector('#incorrect').innerHTML);
+    document.getElementById('incorrect').innerText = ++oldLost;
+}
+
+// Stack overflow
+function startTimer(duration, display) {
+    var timer = duration,
+        minutes,
+        seconds;
+    let interval = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+
+        display.textContent = minutes + ':' + seconds;
+
+        if (--timer < 0) {
+            clearInterval(interval);
+            if (oldScore > oldLost) {
+                document.querySelector('h1').innerHTML = 'You win!';
+            } else if (oldScore < oldLost) {
+                document.querySelector('h1').innerHTML = 'Game Over You lost!';
+            } else {
+                document.querySelector('h1').innerHTML = 'Draw!';
+            }
+        }
+    }, 1000);
+}
+
+let isPlaying = false;
+
+function start() {
+    if (isPlaying) {
+        return;
+    }
+    isPlaying = true;
+    var oneMinute = 10,
+        display = document.querySelector('#span-time');
+    startTimer(oneMinute, display);
 }
